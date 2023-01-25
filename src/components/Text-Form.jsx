e5aba360de51694ba3fde313e767e56ece6e516c
abdullah-raja835 <abdullahaps835@gmail.com>
2023-01-26 00:54:04 +0500

    dark mode done but still bht mistakes hn bugs find kro

--- src/components/Text-Form.jsx
+++ src/components/Text-Form.jsx
@@ -19,24 +19,52 @@ const Form = (props)=>{
         //   let newText = text.toLowerCase();
         setText("");
 }  
-   
+
+const [myStyle,setmyStyle] = useState({
+    color : "black",
+    backgroundColor : "white"
+})
+
+const [btn,setbtn]=useState('Enable Dark Mode')
+
+
+
+const Dark = ()=>{
+ if (setmyStyle.color === "white"){
+    setmyStyle({
+        color : "black",
+        backgroundColor : "white"
+    })
+    setbtn("Enable Dark Mode")
+ }
+else{
+    setmyStyle({
+        color : "white",
+        backgroundColor : "black"
+    })
+    setbtn("Enable Light Mode")
+}
+}
 
 
     return(
         <>
+<div className="container" style={myStyle}>
 
-<div class="mb-3" data-bs-theme="dark">
-  <label for="exampleFormControlTextarea1"  class="form-label"><h1>{props.head}</h1></label>
-  <textarea class="form-control" value={text} onChange={change} id="exampleFormControlTextarea1" rows="8"></textarea>
-  <button className="check" onClick={upperCase}>TO UPPERCASE</button>
-  <button className="check" onClick={lowerCase}>TO LowerCase</button>
-  <button className="check bg-primary " onClick={clearText}>Clear Text</button>
+<div class="mb-3"  >
+  <label for="exampleFormControlTextarea1"  className="form-label"><h1>{props.head}</h1></label>
+  <textarea className="form-control" style={myStyle} value={text} onChange={change} id="exampleFormControlTextarea1" rows="8"></textarea>
+  <button className="check" style={myStyle} onClick={upperCase}>TO UPPERCASE</button>
+  <button className="check" style={myStyle} onClick={lowerCase}>TO LowerCase</button>
+  <button className="check" onClick={Dark} >{btn}</button>
+  <button className="check btn btn-secondary btn-light bg-primary " onClick={clearText}>Clear Text</button>
   <div className="container">
     <h2>Your Text Summary</h2>
-    <p>{text.split().length}words and {text.length} characters </p>
+    <p>{text.split("").length}words and {text.length} characters </p>
     <h2>preview</h2>
     <p>{text}</p>
   </div>
+</div>
 </div>
         </>
     )
